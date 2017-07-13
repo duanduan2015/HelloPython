b9e00b06fe27807b7df414f48e8ff2431fcc0164
Yunduan Han <yunduan.han@gmail.com>
2017-07-13 15:24:27 -0700

    Add Two Sum

---
+++ TwoSum.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dic = {}
+        for i in range(len(nums)):
+            dic[nums[i]] = i
+        for i in range(len(nums)):
+            if (target - nums[i]) in dic:
+                if dic[target - nums[i]] != i:
+                    return (i, dic[target - nums[i]])
